e2cf04fd1439cbbec63bc9f88bb62939da018e16
jshin83 <jen.kyongah.lee@gmail.com>
2019-06-18 15:07:02 -0700

    completed lab, readme, tests

---
+++ README.md
@@ -0,0 +1,14 @@
+# Codefellowship
+
+Allows a new user to make an account, or an existing user to log in to the existing account.
+
+Has default welcome page with links.
+
+**Routes**
+
++ `/` default, homepage
++ `/signup` registration page for new user
++ `/login` login for existing user
++ `/myprofile` profile page for logged in user - cannot access if not logged in
+
+[code](./src/main/java/com.thefuzzydragon.jen.codefellowship)
--- src/test/java/com/thefuzzydragon/jen/codefellowship/CodefellowshipApplicationTests.java
+++ src/test/java/com/thefuzzydragon/jen/codefellowship/CodefellowshipApplicationTests.java
@@ -2,15 +2,56 @@ package com.thefuzzydragon.jen.codefellowship;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CodefellowshipApplicationTests {
+	@Autowired
+	AppUserController controller;
+
+
+	@Autowired
+	MockMvc mockMvc;
+
 
 	@Test
 	public void contextLoads() {
 	}
+
+
+
+	@Test
+	public void testControllerIsAutowired() {
+		assertNotNull(controller);
+	}
+
+
+	@Test
+	public void testHomeRoute() throws Exception{
+		mockMvc.perform(get("/")).andDo(print())
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void testsignupRoute() throws Exception{
+		mockMvc.perform(get("/signup")).andDo(print())
+				.andExpect(status().isOk());
+
+	}
+
+
 
 }
